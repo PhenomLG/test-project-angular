a470edbe3ef5e88195f5bbaa8279b96fde5662b1
Svyatoslav <slavaanufriev@gmail.com>
2023-09-22 21:44:48 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {HeroesComponent} from "./pages/heroes/heroes.component";
+import {DetailComponent} from "./pages/detail/detail.component";
+import {NotfoundComponent} from "./pages/notfound/notfound.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // http://domen.ru/dashboard/10
+  //               //dashboard/:id
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'details/:id', component: DetailComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 import { HeroComponent } from './hero/hero.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import {HeroService} from "./services/hero.service";
+import { DetailComponent } from './pages/detail/detail.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroComponent,
+    DashboardComponent,
+    DetailComponent,
+    NotfoundComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
