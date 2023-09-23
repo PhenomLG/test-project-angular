dee37f6308c440ce3880ca46ec09350ee008ffc3
Svyatoslav <slavaanufriev@gmail.com>
2023-09-23 10:55:21 +0300

    Add dashboard view

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from "../../services/hero.service";
+import {Hero} from "../../models/hero.models";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  topHeroes: Hero[] = [];
+  title: string = 'Top Heroes';
+  constructor(private heroService: HeroService) {
+    //this.topHeroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+    this.topHeroes = this.heroService.getHeroes().slice(0, 4);
+  }
 
 
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,20 +1,21 @@
 import {Hero} from "../models/hero.models";
 
 export class HeroService {
-  constructor() { }
-  heroes: Hero[] = [
+  private heroes: Hero[] = [
     new Hero('Dr.Nice', 12),
     new Hero('Bombasto', 13),
     new Hero('Celeritas', 14),
     new Hero('Magneta', 15),
     new Hero('Poldi', 16),
   ];
+
+  constructor() { }
   getHeroes(): Hero[] {
     return this.heroes;
   }
 
   getHero(id: number): Hero {
-     return this.heroes[this.heroes.findIndex(hero => hero.id = id)];
+     return this.heroes[this.heroes.findIndex(hero => hero.id == id)];
   }
 
   updateHero() {
