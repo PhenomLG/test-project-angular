3b07edac4a30680054a406a6ed6ea1f73939da7d
Svyatoslav <slavaanufriev@gmail.com>
2023-09-23 18:57:08 +0300

    Edit data

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,18 +2,14 @@ import {Hero} from "../models/hero.models";
 
 export class HeroService {
   private heroes: Hero[] = [
-    new Hero('Poldi', 16),
     new Hero('Dr.Nice', 12),
     new Hero('Bombasto', 13),
     new Hero('Celeritas', 14),
     new Hero('Magneta', 15),
-    new Hero('Poldi', 16),
-        new Hero('Dr.Nice', 12),
-    new Hero('Bombasto', 13),
-    new Hero('Celeritas', 14),
-    new Hero('Magneta', 15),
-    new Hero('Poldi', 16),
-
+    new Hero('Dynama', 16),
+    new Hero('Dr.IQ', 16),
+    new Hero('Magma', 16),
+    new Hero('Tornado', 16),
   ];
 
   constructor() { }
@@ -22,6 +18,7 @@ export class HeroService {
   }
 
   getHero(id: number): Hero {
+     // Сделаем допущение, что данные получены с БД и свойство id – primary key
      return this.heroes[this.heroes.findIndex(hero => hero.id == id)];
   }
 }
