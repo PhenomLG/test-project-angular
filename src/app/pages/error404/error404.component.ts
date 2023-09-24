fa6533206d4fd5afa497c87b829d3b067d47e8e7
Svyatoslav <slavaanufriev@gmail.com>
2023-09-24 19:57:46 +0300

    More informative errors

--- src/app/pages/error404/error404.component.ts
+++ src/app/pages/error404/error404.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-error404',
   template: `
     <section class="error">
-      <h2>The page is not found</h2>
+      <h2>{{errorMessage}}</h2>
       <img src="../../../assets/error.gif" alt="Error" height="280" width="280">
     </section>'
   `
 })
-export class Error404Component { }
+export class Error404Component {
+  @Input() errorMessage: string = 'The page is not found';
+}
